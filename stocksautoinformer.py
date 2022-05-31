e97f64a5a0a184b6cbc92edaf16c927cde39be1b
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2022-05-31 21:09:38 +0300

    Update stocksautoinformer.py

--- stocksautoinformer.py
+++ stocksautoinformer.py
@@ -1,11 +1,25 @@
 import os
+from matplotlib import ticker
+
+import yfinance
+PORTFOLIO = ["GPS", "KO", "AAPL", "RWT", "AEO", "T" ]
+
+def getlistoftickers(listofstocknames):
+    yfobjects = []
+    for stock in listofstocknames:
+        yfobjects.append(yfinance.Ticker(stock))
+    return yfobjects
 
 def foldercreation(listofstocknames):
     """
+    Creates a folder for each element of the list
     Args:
         listofstocknames: a list of stock names
     """
     for stock in listofstocknames:
         os.mkdir(stock)
-        os.chdir("..")
+
+
+foldercreation(PORTFOLIO)
+tickers = getlistoftickers(PORTFOLIO)
 
