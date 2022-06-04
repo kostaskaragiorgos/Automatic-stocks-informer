a009e72ebb943c0a81df6da93067f2bd8320da22
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2022-06-04 15:43:44 +0300

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -3,6 +3,16 @@ class Portfolio():
     def __init__(self, name):
         self.name = name
         self.tickers = {}
+
     def setname(self, name):
         self.name = name
+    
+    def getname(self):
+        return self.name
+    
+    def add_ticker(self, nameofthestock, tickerofthestock):
+        self.tickers = {nameofthestock: yf.Ticker(tickerofthestock)}
+    
+
+
 
