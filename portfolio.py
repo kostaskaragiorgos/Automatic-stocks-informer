d95bb2d76903d7b58941b8ea396b6afe1ebfa5ec
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2022-06-04 15:33:38 +0300

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -1,6 +1,8 @@
+import yfinance as yf
 class Portfolio():
     def __init__(self, name):
         self.name = name
         self.tickers = {}
-    
+    def setname(self, name):
+        self.name = name
 
