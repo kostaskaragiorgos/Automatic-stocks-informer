44073110ef1054e8d228afae7ced766a43bd494e
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2022-06-10 17:14:32 +0300

    Update mypdf.py

--- mypdf.py
+++ mypdf.py
@@ -6,3 +6,8 @@ class PDF(FPDF):
         self.set_font('Arial', 'B',size=15)
         self.cell(w=0,h=0,txt=name,align='C')
         self.ln(10)
+    
+    def add_text(self, text):
+        self.set_font('Arial',size=10)
+        self.write(5,text)
+        self.ln(10)
