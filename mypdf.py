daa5086250682f19e5e6156e3edbfa0a5ef7e84d
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2022-06-10 17:15:41 +0300

    Update mypdf.py

--- mypdf.py
+++ mypdf.py
@@ -16,3 +16,14 @@ class PDF(FPDF):
         self.image(image_name)
         self.ln(10)
     
+    def add_csvfile(self, csv_file_name):
+        page_width = self.w - 2 * pdf.l_margin
+        col_width = page_width/4
+        th = 14
+        with open(csv_file_name) as f:
+            reader = csv.reader(f)
+            for row in reader:
+                for i in range(len(row)):
+                    self.cell(col_width, th, row[i], border=1)
+                self.ln(th)
+            self.ln(10)
